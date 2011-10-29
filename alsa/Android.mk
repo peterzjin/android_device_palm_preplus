eedc0882e2c16fdd129426bae1770a366aeee3ea
Peter Jin <peter.zjin@gmail.com>
2011-10-29 23:40:01 +0800

    audio: add audio HAL code (alsa.preplus.so)
    just an initial version, only speaker and headset supported.
    
    Change-Id: Ie55fb41a10283e6092c11d3b5228cd7615553139
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

---
+++ alsa/Android.mk
@@ -0,0 +1,32 @@
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+
+  LOCAL_PATH := $(call my-dir)
+
+  include $(CLEAR_VARS)
+
+  LOCAL_PRELINK_MODULE := false
+
+  LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+  LOCAL_CFLAGS := -D_POSIX_SOURCE -Wno-multichar
+
+ifneq ($(ALSA_DEFAULT_SAMPLE_RATE),)
+    LOCAL_CFLAGS += -DALSA_DEFAULT_SAMPLE_RATE=$(ALSA_DEFAULT_SAMPLE_RATE)
+endif
+
+  LOCAL_C_INCLUDES += \
+      external/alsa-lib/include \
+      hardware/alsa_sound
+
+  LOCAL_SRC_FILES := alsa_preplus.cpp
+
+  LOCAL_SHARED_LIBRARIES := \
+      libasound \
+      liblog
+
+  LOCAL_MODULE:= alsa.preplus
+  LOCAL_MODULE_TAGS:= optional
+
+  include $(BUILD_SHARED_LIBRARY)
+
+endif
--- preplus.mk
+++ preplus.mk
@@ -22,4 +22,33 @@ PRODUCT_NAME := generic_preplus
 PRODUCT_DEVICE := preplus
 
 PRODUCT_COPY_FILES += \
-    device/palm/preplus/init.rc:root/init.rc
+    device/palm/preplus/init.rc:root/init.rc \
+    device/palm/preplus/init.preplus.rc:root/init.preplus.rc
+
+# audio scripts from webos
+PRODUCT_COPY_FILES += \
+    device/palm/preplus/etc/audio/default.txt:system/etc/audio/default.txt \
+    device/palm/preplus/etc/audio/dtmf.txt:system/etc/audio/dtmf.txt \
+    device/palm/preplus/etc/audio/media_a2dp.txt:system/etc/audio/media_a2dp.txt \
+    device/palm/preplus/etc/audio/media_back_speaker.txt:system/etc/audio/media_back_speaker.txt \
+    device/palm/preplus/etc/audio/media_front_speaker.txt:system/etc/audio/media_front_speaker.txt \
+    device/palm/preplus/etc/audio/media_headset_mic.txt:system/etc/audio/media_headset_mic.txt \
+    device/palm/preplus/etc/audio/media_headset.txt:system/etc/audio/media_headset.txt \
+    device/palm/preplus/etc/audio/media_wireless.txt:system/etc/audio/media_wireless.txt \
+    device/palm/preplus/etc/audio/phone_back_speaker.txt:system/etc/audio/phone_back_speaker.txt \
+    device/palm/preplus/etc/audio/phone_bluetooth_sco.txt:system/etc/audio/phone_bluetooth_sco.txt \
+    device/palm/preplus/etc/audio/phone_front_speaker.txt:system/etc/audio/phone_front_speaker.txt \
+    device/palm/preplus/etc/audio/phone_headset_mic.txt:system/etc/audio/phone_headset_mic.txt \
+    device/palm/preplus/etc/audio/phone_headset.txt:system/etc/audio/phone_headset.txt \
+    device/palm/preplus/etc/audio/phone_tty_full.txt:system/etc/audio/phone_tty_full.txt \
+    device/palm/preplus/etc/audio/phone_tty_hco.txt:system/etc/audio/phone_tty_hco.txt \
+    device/palm/preplus/etc/audio/phone_tty_vco.txt:system/etc/audio/phone_tty_vco.txt \
+    device/palm/preplus/etc/audio/scenarios.xml:system/etc/audio/scenarios.xml \
+    device/palm/preplus/etc/audio/voice_command_back_speaker.txt:system/etc/audio/voice_command_back_speaker.txt \
+    device/palm/preplus/etc/audio/voice_command_bluetooth_sco.txt:system/etc/audio/voice_command_bluetooth_sco.txt \
+    device/palm/preplus/etc/audio/voice_command_headset_mic.txt:system/etc/audio/voice_command_headset_mic.txt \
+    device/palm/preplus/etc/audio/voice_command_headset.txt:system/etc/audio/voice_command_headset.txt
+
+#
+PRODUCT_PACKAGES += \
+    alsa.preplus

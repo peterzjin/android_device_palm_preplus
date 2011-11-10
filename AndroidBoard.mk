9b73c5b25c3918f5aa19794b922426ca9c6a63d2
Peter Jin <peter.zjin@gmail.com>
2011-11-10 21:38:24 +0800

    FIXME: use a ext3 image to mount userdata
    Change-Id: I76c2ff18eafc05c16bc8217082836ace81ca0405
    Signed-off-by: Peter Jin <peter.zjin@gmail.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -9,3 +9,9 @@ file := $(TARGET_OUT_KEYLAYOUT)/maxim_keypad.kl
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/maxim_keypad.kl | $(ACP)
 	$(transform-prebuilt-to-target)
+
+preplus: droid
+ifeq ($(TARGET_USE_EXT3_USERDATA_IMG),true)
+	dd if=/dev/zero of=$(PRODUCT_OUT)/userdata.ext3 bs=1MiB count=48
+	mkfs.ext3 -F $(PRODUCT_OUT)/userdata.ext3
+endif
